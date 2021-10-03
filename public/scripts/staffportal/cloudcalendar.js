5c97d433546987f241baa543f173e8bebcbd95f5
mtisby <marieltisby1998@gmail.com>
2021-10-03 16:29:05 -0700

    google calendar apis

--- public/scripts/staffportal/cloudcalendar.js
+++ public/scripts/staffportal/cloudcalendar.js
@@ -1,5 +1,17 @@
 import { google } from "googleapis";
 import { DotenvConfigOptions } from "dotenv";
 
+// get credentials 
 const credentials = JSON.parse(process.env.CREDENTIALS);
 const calendarId = process.env.CALENDAR_ID;
+
+// Google calendar API settings
+const SCOPES = 'https://www.googleapis.com/auth/calendar';
+const calendar = google.calendar({version : "v3"});
+
+const auth = new google.auth.JWT(
+    CREDENTIALS.client_email,
+    null,
+    CREDENTIALS.private_key,
+    SCOPES
+);
