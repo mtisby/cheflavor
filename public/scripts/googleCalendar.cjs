64d01474286364b059eee0905629cb8dc25b7cc2
mtisby <marieltisby1998@gmail.com>
2021-11-02 15:28:31 -0700

    update calendar

--- public/scripts/googleCalendar.cjs
+++ public/scripts/googleCalendar.cjs
@@ -1,6 +1,6 @@
 // Require google from googleapis package.
 const { google } = require('googleapis')
-require('dotenv').config({ path: ".env" })
+require('dotenv').config({ path: "../../.env" })
 
 // Require oAuth2 from our google instance.
 const { OAuth2 } = google.auth
